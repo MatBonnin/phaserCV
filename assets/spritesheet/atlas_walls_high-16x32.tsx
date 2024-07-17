4c32f2e4f7266ff6d0a3cdcfad8f1510f7e13700
MatBonnin <mbonnin38@gmail.com>
2024-07-17 17:42:42 +0200

    Updates

---
+++ assets/spritesheet/atlas_walls_high-16x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="atlas_walls_high-16x32" tilewidth="16" tileheight="16" tilecount="192" columns="24">
+ <image source="gfx/0x72_DungeonTilesetII_v1.7/0x72_DungeonTilesetII_v1.7/atlas_walls_high-16x32.png" width="384" height="128"/>
+</tileset>
