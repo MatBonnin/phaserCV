dc066169c4f74314d902969c2863fa2c061c17c8
MatBonnin <mbonnin38@gmail.com>
2024-07-18 13:47:40 +0200

    test autoSize

--- main.js
+++ main.js
@@ -6,11 +6,39 @@ import HouseScene from './scenes/HouseScene';
 import Level1Scene from './scenes/Level1Scene';
 import Phaser from 'phaser';
 
+// Fonction pour calculer la taille idéale du jeu
+const calculateIdealSize = (screenWidth, screenHeight) => {
+  const idealWidth = 600;
+  const idealHeight = 600;
+  const referenceWidth = 2560;
+  const referenceHeight = 1271;
+
+  // Calculer le ratio
+  const widthRatio = screenWidth / referenceWidth;
+  const heightRatio = screenHeight / referenceHeight;
+
+  // Utiliser le plus petit ratio pour maintenir l'aspect
+  const ratio = Math.min(widthRatio, heightRatio);
+
+  // Calculer les dimensions idéales
+  const gameWidth = Math.floor(idealWidth * ratio);
+  const gameHeight = Math.floor(idealHeight * ratio);
+  console.log(gameWidth, gameHeight);
+  return { gameWidth, gameHeight };
+};
+
+// Obtenir la taille de l'écran
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+// Calculer la taille idéale du jeu
+const { gameWidth, gameHeight } = calculateIdealSize(screenWidth, screenHeight);
+
 const config = {
   type: Phaser.WEBGL,
   parent: 'gameCanvas',
-  width: 600, // Taille initiale de votre jeu
-  height: 600, // Taille initiale de votre jeu
+  width: gameWidth,
+  height: gameHeight,
   physics: {
     default: 'arcade',
     arcade: {
@@ -20,8 +48,18 @@ const config = {
   },
   scene: [GameScene, HouseScene, CaveScene, Level1Scene],
   scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
     zoom: 3, // Désactivez le redimensionnement automatique
   },
 };
 
 const game = new Phaser.Game(config);
+
+// Ajouter un écouteur d'événement pour redimensionner le jeu lorsque la fenêtre est redimensionnée
+window.addEventListener('resize', () => {
+  const { gameWidth, gameHeight } = calculateIdealSize(
+    window.innerWidth,
+    window.innerHeight
+  );
+  game.scale.resize(gameWidth, gameHeight);
+});
