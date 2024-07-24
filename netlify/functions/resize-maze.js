78b7215cb998700260b6a70035a687dbb48fcfd1
MatBonnin <mbonnin38@gmail.com>
2024-07-24 09:52:27 +0200

    onyretourne

--- netlify/functions/resize-maze.js
+++ netlify/functions/resize-maze.js
@@ -90,8 +90,14 @@ exports.handler = async (event, context) => {
       for (const row of transformedLabyrinth) {
         finalString += row.join(',') + ',';
       }
-      const filePath = 'laby.tmj';
+      const filePath = path.join(
+        process.cwd(),
+        'public/assets/map',
+        'laby.tmj'
+      );
 
+      console.log('Current working directory:', process.cwd());
+      console.log('Resolved filePath:', filePath);
       try {
         let read = await fs.readFile(filePath, 'utf8');
 
